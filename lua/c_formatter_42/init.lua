1d38a8f9c4b38480fea16cec0493d27005260ce3
Itsoon <91259186+Itsoon@users.noreply.github.com>
2025-04-30 20:32:56 +0200

    Update init.lua

--- lua/c_formatter_42/init.lua
+++ lua/c_formatter_42/init.lua
@@ -1,20 +1,13 @@
-vim.keymap.set("n", "<leader>uf", function()
-	_G.enable_format_on_save = not _G.enable_format_on_save
-
-	if _G.enable_format_on_save then
-		vim.notify("Format on save: ENABLED", vim.log.levels.INFO)
-	else
-		vim.notify("Format on save: DISABLED", vim.log.levels.WARN)
-	end
-end, { desc = "Toggle format on save" })
-
 local M = {}
+
+_G.enable_format_on_save = false
 
 function M.format()
 	local buf = vim.api.nvim_get_current_buf()
 	local filepath = vim.api.nvim_buf_get_name(buf)
 
 	if filepath == "" or vim.bo[buf].filetype ~= "c" then
+		vim.notify("Not a .c file", vim.log.levels.WARN)
 		return
 	end
 
@@ -22,22 +15,54 @@ function M.format()
 	vim.fn.jobstart(cmd, {
 		on_exit = function(_, code)
 			if code == 0 then
-				vim.cmd("edit!") -- Reload the file
+				vim.api.nvim_command("edit!") -- Reload the file after formatting
 				vim.notify("Formatted with c_formatter_42", vim.log.levels.INFO)
 			else
-				vim.notify("c_formatter_42 failed", vim.log.levels.ERROR)
+				vim.notify("Formatting failed", vim.log.levels.ERROR)
+			end
+		end,
+		stderr_buffered = true,
+		stdout_buffered = true,
+	})
+end
+
+function M.setup(opts)
+	opts = opts or {}
+	local auto_format = opts.auto_format or false
+	local toggle_key = opts.toggle_key or "<leader>uf" -- Default toggle key
+
+	vim.api.nvim_create_user_command("FormatC42", function()
+		M.format()
+	end, {})
+
+	if auto_format then
+		M.enable_format_on_save()
+	end
+
+	vim.keymap.set("n", toggle_key, function()
+		M.toggle_format_on_save()
+	end, { desc = "Toggle format on save" })
+end
+
+function M.enable_format_on_save()
+	vim.api.nvim_create_autocmd("BufWritePre", {
+		pattern = "*.c",
+		callback = function()
+			if _G.enable_format_on_save then
+				M.format()
 			end
 		end,
 	})
 end
 
-vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*.c",
-	callback = function()
-		if _G.enable_format_on_save then
-			M.format()
-		end
-	end,
-})
+function M.toggle_format_on_save()
+	_G.enable_format_on_save = not _G.enable_format_on_save
+
+	if _G.enable_format_on_save then
+		vim.notify("Format on save: ENABLED", vim.log.levels.INFO)
+	else
+		vim.notify("Format on save: DISABLED", vim.log.levels.WARN)
+	end
+end
 
 return M
