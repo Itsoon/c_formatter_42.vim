e2efd1ecd4b21de8227ebb3072444e978cbed78d
Itsoon <91259186+Itsoon@users.noreply.github.com>
2025-08-07 01:02:06 +0200

    add venv

--- lua/c_formatter_42/init.lua
+++ lua/c_formatter_42/init.lua
@@ -3,40 +3,38 @@ local M = {}
 _G.enable_format_on_save = false
 
 local function ensure_c_formatter_42_installed()
-	if vim.fn.executable("c_formatter_42") == 1 then
-		return true
+	local home = vim.fn.expand("~")
+	local install_dir = home .. "/.local/src/c_formatter_42"
+	local venv_path = install_dir .. "/venv"
+	local bin_path = venv_path .. "/bin"
+	local formatter_path = bin_path .. "/c_formatter_42"
+
+	if vim.fn.executable(formatter_path) == 1 then
+		return formatter_path
 	end
 
-	vim.notify("Installing c_formatter_42...", vim.log.levels.INFO)
+	vim.notify("Installing c_formatter_42 in virtualenv...", vim.log.levels.INFO)
 
-	local install_dir = vim.fn.expand("~/.local/src/c_formatter_42")
 	if vim.fn.isdirectory(install_dir) == 0 then
 		vim.fn.mkdir(install_dir, "p")
 	end
 
-	local install_cmd = string.format(
-		[[
-		git clone https://github.com/cacharle/c_formatter_42 %s && \
-		pip3 install --user -e %s
-	]],
-		install_dir,
-		install_dir
-	)
+	local cmds = {
+		string.format("git clone https://github.com/cacharle/c_formatter_42 %s", install_dir),
+		string.format("python -m venv %s", venv_path),
+		string.format("%s/pip install -e %s", bin_path, install_dir),
+	}
 
-	local result = vim.fn.system(install_cmd)
-
-	if vim.v.shell_error ~= 0 then
-		vim.notify("Failed to install c_formatter_42:\n" .. result, vim.log.levels.ERROR)
-		return false
+	for _, cmd in ipairs(cmds) do
+		local result = vim.fn.system(cmd)
+		if vim.v.shell_error ~= 0 then
+			vim.notify("Installation step failed:\n" .. result, vim.log.levels.ERROR)
+			return nil
+		end
 	end
 
-	local local_bin = vim.fn.expand("~/.local/bin")
-	if not string.find(vim.env.PATH, local_bin, 1, true) then
-		vim.env.PATH = vim.env.PATH .. ":" .. local_bin
-	end
-
-	vim.notify("Successfully installed c_formatter_42", vim.log.levels.INFO)
-	return true
+	vim.notify("Successfully installed c_formatter_42 in virtualenv", vim.log.levels.INFO)
+	return formatter_path
 end
 
 function M.format()
@@ -48,11 +46,16 @@ function M.format()
 		return
 	end
 
-	local cmd = string.format("c_formatter_42 %s", vim.fn.shellescape(filepath))
+	local formatter = ensure_c_formatter_42_installed()
+	if not formatter then
+		return
+	end
+
+	local cmd = string.format("%s %s", formatter, vim.fn.shellescape(filepath))
 	vim.fn.jobstart(cmd, {
 		on_exit = function(_, code)
 			if code == 0 then
-				vim.cmd("edit!") -- Reload the file after formatting
+				vim.cmd("edit!")
 				vim.notify("Formatted with c_formatter_42", vim.log.levels.INFO)
 			else
 				vim.notify("Formatting failed", vim.log.levels.ERROR)
@@ -64,10 +67,6 @@ function M.format()
 end
 
 function M.setup(opts)
-	if not ensure_c_formatter_42_installed() then
-		return
-	end
-
 	opts = opts or {}
 	local auto_format = opts.auto_format or false
 	local toggle_key = opts.toggle_key or "<leader>uf"
