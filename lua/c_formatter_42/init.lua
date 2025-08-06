df3a95de616ccc3c657bdd8568d1be7e467f0f53
Itsoon <91259186+Itsoon@users.noreply.github.com>
2025-08-07 01:00:24 +0200

    ensure c_formatter_42 is installed

--- lua/c_formatter_42/init.lua
+++ lua/c_formatter_42/init.lua
@@ -2,6 +2,43 @@ local M = {}
 
 _G.enable_format_on_save = false
 
+local function ensure_c_formatter_42_installed()
+	if vim.fn.executable("c_formatter_42") == 1 then
+		return true
+	end
+
+	vim.notify("Installing c_formatter_42...", vim.log.levels.INFO)
+
+	local install_dir = vim.fn.expand("~/.local/src/c_formatter_42")
+	if vim.fn.isdirectory(install_dir) == 0 then
+		vim.fn.mkdir(install_dir, "p")
+	end
+
+	local install_cmd = string.format(
+		[[
+		git clone https://github.com/cacharle/c_formatter_42 %s && \
+		pip3 install --user -e %s
+	]],
+		install_dir,
+		install_dir
+	)
+
+	local result = vim.fn.system(install_cmd)
+
+	if vim.v.shell_error ~= 0 then
+		vim.notify("Failed to install c_formatter_42:\n" .. result, vim.log.levels.ERROR)
+		return false
+	end
+
+	local local_bin = vim.fn.expand("~/.local/bin")
+	if not string.find(vim.env.PATH, local_bin, 1, true) then
+		vim.env.PATH = vim.env.PATH .. ":" .. local_bin
+	end
+
+	vim.notify("Successfully installed c_formatter_42", vim.log.levels.INFO)
+	return true
+end
+
 function M.format()
 	local buf = vim.api.nvim_get_current_buf()
 	local filepath = vim.api.nvim_buf_get_name(buf)
@@ -15,7 +52,7 @@ function M.format()
 	vim.fn.jobstart(cmd, {
 		on_exit = function(_, code)
 			if code == 0 then
-				vim.api.nvim_command("edit!") -- Reload the file after formatting
+				vim.cmd("edit!") -- Reload the file after formatting
 				vim.notify("Formatted with c_formatter_42", vim.log.levels.INFO)
 			else
 				vim.notify("Formatting failed", vim.log.levels.ERROR)
@@ -27,6 +64,10 @@ function M.format()
 end
 
 function M.setup(opts)
+	if not ensure_c_formatter_42_installed() then
+		return
+	end
+
 	opts = opts or {}
 	local auto_format = opts.auto_format or false
 	local toggle_key = opts.toggle_key or "<leader>uf"
@@ -41,7 +82,6 @@ function M.setup(opts)
 		M.enable_format_on_save()
 	end
 
-	-- Keymap pour toggle
 	vim.keymap.set("n", toggle_key, function()
 		M.toggle_format_on_save()
 	end, { desc = "Toggle format on save" })
